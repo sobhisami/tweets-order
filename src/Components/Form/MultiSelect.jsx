b743fb61d4554982fad4c5bde53100abc92f2d41
sobhisami <sobhisami073@gmail.com>
2023-09-04 11:25:41 +0300

    tr

--- src/Components/Form/MultiSelect.jsx
+++ src/Components/Form/MultiSelect.jsx
@@ -6,53 +6,61 @@ const animatedComponents = makeAnimated();
 
 export let MultiSelect = ({ menuOptions, onChange, value }) => {
   const [quantities, setQuantities] = useState({});
-
-  const handleChange = (selectedValues) => {
-    const newQuantities = {};
-    selectedValues.forEach((option) => {
-      newQuantities[option.value] = option.quantity || 0;
-    });
-    setQuantities(newQuantities);
-
-    onChange(selectedValues);
+  const increase = (e, itemId) => {
+    e.preventDefault();
+    e.stopPropagation(); 
+    setQuantities(prevQuantities => ({
+      ...prevQuantities,
+      [itemId]: (prevQuantities[itemId] || 0) + 1,
+    }));
   };
 
-  const multiSelectOptions = menuOptions.map((item) => ({
+  const decrease = (e, itemId) => {
+    e.preventDefault(); 
+    e.stopPropagation(); 
+    if (quantities[itemId] && quantities[itemId] > 0) {
+      setQuantities(prevQuantities => ({
+        ...prevQuantities,
+        [itemId]: prevQuantities[itemId] - 1,
+      }));
+    }
+  };
+
+  const multiSelectOptions = menuOptions.map(item => ({
     value: item.id,
-    label: `${item.name} - ${item.price} ش`,
+    label: (
+      <div>
+        {`${item.name} - ${item.price} ش`}
+        <button onClick={(e) => increase(e, item.id)}>+</button>
+        <span>{quantities[item.id] || 1}</span>
+        <button onClick={(e) => decrease(e, item.id)}>-</button>
+      </div>
+    ),
     product: item.name,
     price: item.price,
     quantity: quantities[item.id] || 1,
   }));
 
-  const customStyles = {
-    control: (base) => ({
-      ...base,
-      direction: 'rtl', 
-    }),
-  };
 
   return (
     <Select
       options={multiSelectOptions}
       components={animatedComponents}
       isMulti
-      onChange={handleChange}
+      onChange={onChange}
       value={value}
-      isSearchable
-      styles={customStyles} 
     />
   );
 };
 
 export const calculateTotalPrice = (selectedOptions) => {
   if (!Array.isArray(selectedOptions)) {
-    return 0;
+    return 0; 
   }
 
   let totalPrice = 0;
-  selectedOptions.forEach((option) => {
-    totalPrice += option.price * (option.quantity || 1);
+  selectedOptions.forEach(option => {
+    totalPrice += option?.price * ((option?.quantity ?? 0));
   });
   return totalPrice;
 };
